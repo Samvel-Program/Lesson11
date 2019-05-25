9d47dd8fbfd60089f57fec915874c38614a0d501
Samvel Akopyan <samvel.mgsu@mail.ru>
2019-05-25 18:38:21 +0300

    commit

---
+++ src/Program.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+/**
+ * Created by OUT-Akopyan-SR on 24.05.2019.
+ */
+public class Program {
+
+    public static void main(String[] args) {
+        System.out.println("Введите текст");
+        Scanner in = new Scanner(System.in);
+        String word = in.nextLine();
+        System.out.println(word);
+    }
+
+
+
+}
+
+
